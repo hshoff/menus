ce2e00c94f37b1f44958e235bc9e022be30b601c
Harrison Shoff <hi@hshoff.com>
2013-11-23 16:59:41 -0800

    [nested][index] add cid and grease lightning..it's snowing out

--- nested/js/menu-nested.js
+++ nested/js/menu-nested.js
@@ -1,4 +1,16 @@
 !function(exports) {
   'use strict';
 
+  var idCounter = 0;
+
+  function uniqueId(prefix) {
+    var id = ++idCounter + '';
+    return prefix ? prefix + id : id;
+  }
+
+  function Nested(options) {
+    this.cid = uniqueId('nested');
+    console.log(this.cid);
+  }
+
 }(this);
